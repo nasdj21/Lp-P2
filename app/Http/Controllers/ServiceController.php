d9c882149898db9ca1eca92d635b1d88e60439df
nasdj21 <nasierra@espol.edu.ec>
2026-01-11 20:46:30 -0500

    Creación y opcion de mostrar un servicio

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ServiceController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource. (GET /api/services)
      */
     public function index()
     {
@@ -45,11 +45,34 @@ class ServiceController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Crear un nuevo servicio (POST /api/services)
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string|max:255|unique:service,name',
+            'price' => 'required|numeric|get:0',
+            'professionals' => 'sometimes|array'
+            'professionals.*' => 'exists:professional,person_id',
+            'created_by' => 'nullable|string|max:255',
+        ]);
+
+        //Extraer profesionales y eliminar del array validado
+        $professionalIds = $validated['professionals'] ?? [];
+        unset($validated['professionals']);
+
+        //crear servicio
+        $service = Service::create($valited);
+
+        // Asociar profesionales is es que se enviaron
+        if (!empty($professionalIds)){
+            $service->professionals()->attach($professionalIds);
+        }
+
+        return response()->jsoon([
+            'message' => 'Service created successfully',
+            'service' => $service->load(['professioanls.person.userAccount'])
+        ], 201);
     }
 
     /**

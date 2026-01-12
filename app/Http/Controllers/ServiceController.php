29d64c0d52642bf743fd40218a1c4a20018a3ed4
nasdj21 <nasierra@espol.edu.ec>
2026-01-11 20:29:02 -0500

    Controlador de servicio actualziado

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Service;
+use App\Models\Professional;
 
 use Illuminate\Http\Request;
 
@@ -11,7 +13,35 @@ class ServiceController extends Controller
      */
     public function index()
     {
-        //
+        // Cargar servicios con profesionales (y sus personas/usuarios) y pagos
+        $services = Service::with([
+            'professionals.person.userAccount',
+            'payments'
+        ])->get();
+
+        // Transformar colección a estructura descriptiva
+        return response()->json($services->map(function($service){
+            return [
+                'service_id' => $service->service_id,
+                'name' => $service->name,
+                'price' => $service->price,
+                'professionals' => $service->professionals->map(function($professional){
+                   return [
+                    'person_id' => $professional->person_id,
+                    'first_name' => $professional->person->first_name ?? null,
+                    'last_name' => $professional->person->last_name ?? null,
+                    'email' => $professional->person->userAccount->email ?? null,
+                    'title' => $professional->title ?? null,
+                    'specialty' => $professional->specialty ?? null,
+                   ]; 
+                }),
+                'payments_count' => $service->payments->count(),
+                'created_by' => $service->created_by,
+                'creation_date' => $service->creation_date,
+                'modified_by' => $service->modified_by,
+                'modification_date' => $service->modification_date,
+            ];
+        }));
     }
 
     /**
